cdbd74d6bbebdd0ac0522184649ab5b1ae86abf4
ThatDraenGuy <haykin.oleg@rambler.ru>
2026-02-13 20:09:27 +0300

    build: workflows added

--- build.rs
+++ build.rs
@@ -2,14 +2,14 @@ fn main() {
     use std::path::Path;
     const PARSE_Y: &str = "src/parser/parse.y";
 
-    println!("cargo:rerun-if-changed={}", PARSE_Y);
+    println!("cargo:rerun-if-changed={PARSE_Y}");
     println!("Generating parse.rs");
 
     match rust_bison_skeleton::process_bison_file(Path::new(PARSE_Y)) {
-        Ok(_) => {}
+        Ok(_) => {},
         Err(err) => {
-            eprintln!("Failed to generate grammar.\n{:#?}", err);
+            eprintln!("Failed to generate grammar.\n{err:#?}");
             std::process::exit(1);
-        }
+        },
     }
 }
